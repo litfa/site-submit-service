d8c5820a5cd5afc4ac1a8a148b4ad4f28a6c7633
Litfa <admin@litf.com.cn>
2022-04-14 13:45:57 +0800

    feat: 登录验证

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-04-13 17:25:45
- * @LastEditTime: 2022-04-13 20:25:56
+ * @LastEditTime: 2022-04-13 20:41:43
  * @LastEditors: litfa
  * @Description: 配置文件
  * @FilePath: /service/src/config/config.ts
@@ -10,5 +10,7 @@
 export default {
   port: 3001,
   apiBaseUrl: '/api',
-  jwtSecretKey: 'abc'
+  jwtSecretKey: 'abc',
+  app_id: '4657007147bd4995c2625fec',
+  token: 'cdcee7a508e0b66e7cda6cc83a624ae9'
 }
--- src/router/login.ts
+++ src/router/login.ts
@@ -1,17 +1,58 @@
 /*
  * @Author: litfa
  * @Date: 2022-04-13 20:06:24
- * @LastEditTime: 2022-04-13 20:32:18
+ * @LastEditTime: 2022-04-14 13:45:01
  * @LastEditors: litfa
  * @Description: 登录
  * @FilePath: /service/src/router/login.ts
  * 
  */
 import { Router } from 'express';
+import axios from 'axios'
+import config from './../config/config'
+import md5 from 'md5'
 const router = Router()
 
-router.post('/', (req, res) => {
+const createSign = (data: any, token: string): string => {
+  // 1. 将所有参数的key按照字符串进行排序
+  let key = ''
+  for (let i in data) {
+    // 2. 如果参数的value不为空，则将key和value以=号链接，且在后面增加&，例如参数r 为 1Nm882l7， 则拼接为r=1Nm882l7&
+    if (data[i]) {
+      // 3. 按照排序的顺序拼接所有的参数
+      key += `${i}=${data[i]}&`
+    }
+  }
+  // 4. 拼接完成后，在末尾拼接 token=密钥Token， 其中密钥Token可在用户中心查看，请注意密钥Token不要泄露。
+  key += `token=${config.token}`
+
+  // 5. 对拼接后的字符串进行 md5 加密，然后全部转为小写
+  return md5(key).toLowerCase()
+}
+
+router.post('/', async (req, res) => {
   const { mobile, token } = req.body
+  const data = {
+    app_id: config.app_id,
+    id: token,
+    mobile,
+    r: Math.random().toString()
+  }
+  const key = createSign(data, config.token)
+  console.log({
+    ...data,
+    key
+  });
+
+  const { data: results } = await axios({
+    url: 'https://api.jijiancode.com/api/s/third/verify_id',
+    method: 'POST',
+    data: {
+      ...data,
+      key
+    }
+  })
+  console.log(results)
   res.send('111')
 })
 
